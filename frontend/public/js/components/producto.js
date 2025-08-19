9213f7561f11316309f144625457f42cab6435a5
Jeferson Duque Ocampo <jefersonduque3222@gmail.com>
2025-08-18 21:35:01 -0500

    Componente card

---
+++ frontend/public/js/components/producto.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buyButton = document.querySelector(".Comprar__button");
+  buyButton.addEventListener("click", function () {
+    alert("Has agregado un kilo de tomate al carrito");
+  });
+});
