2b414421e68fcc1cea045b9fb16c09f786423455
Juan Jose Ocampo <jujocampore154@gmail.com>
2025-09-02 08:35:10 -0500

    fixOcampo: Se cambian los nombres y variables al ingles

--- frontend/public/js/components/publication_gestor.js
+++ frontend/public/js/components/publication_gestor.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function (){
-    const direction = document.querySelector(".agregar_direccion");
+    const direction = document.querySelector(".publications-gestor");
 
     if(direction){
-      fetch("/frontend/public/views/components/agregar_direccion.html")
+      fetch("/frontend/public/views/components/publications_gestor.html")
         .then(response => response.text())
         .then(data => {
           direction.innerHTML = data;
