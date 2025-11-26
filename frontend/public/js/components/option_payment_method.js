a23d5f69359d3318cfc1b5d4b4328a47cfa115a5
GitMg0 <miguelgrajales9801@gmail.com>
2025-11-26 18:10:51 -0500

    style: cambios en estilos de componentes

--- frontend/public/js/components/option_payment_method.js
+++ frontend/public/js/components/option_payment_method.js
@@ -2,36 +2,40 @@ document.addEventListener('DOMContentLoaded', function () {
   const methodContainer = document.querySelector('.payment-method-container');
 
   if (methodContainer) {
-    fetch("/frontend/public/views/components/option_payment_method.html")
-      .then(response => {
-        if (!response.ok) throw new Error("Error al cargar option_payment_method.html");
+    fetch('/frontend/public/views/components/option_payment_method.html')
+      .then((response) => {
+        if (!response.ok)
+          throw new Error('Error al cargar option_payment_method.html');
         return response.text();
       })
-      .then(data => {
+      .then((data) => {
         methodContainer.innerHTML = data;
 
         // === Opciones de método de pago ===
-        const options = methodContainer.querySelectorAll(".payment-method__option");
-        const payButton = methodContainer.querySelector(".payment-method__pay"); // 🔥 Botón pagar
+        const options = methodContainer.querySelectorAll(
+          '.payment-method__option'
+        );
+        const payButton = methodContainer.querySelector('.payment-method__pay'); // 🔥 Botón pagar
 
-        options.forEach(option => {
-          option.addEventListener("click", () => {
+        options.forEach((option) => {
+          option.addEventListener('click', () => {
             // Quita la selección de todas
-            options.forEach(o => o.classList.remove("selected"));
+            options.forEach((o) => o.classList.remove('selected'));
             // Marca la que clicaste
-            option.classList.add("selected");
+            option.classList.add('selected');
           });
         });
 
         // === 🔥 Evento: botón "Pagar" redirige a details.html ===
         if (payButton) {
-          payButton.addEventListener("click", () => {
-            window.location.href = "view_client_  order.html";
+          payButton.addEventListener('click', () => {
+            window.location.href = 'view_client_order.html';
           });
         }
-
       })
-      .catch(error => console.error("Error cargando el componente Payment Method:", error));
+      .catch((error) =>
+        console.error('Error cargando el componente Payment Method:', error)
+      );
   } else {
     console.warn("No se encontró '.payment-method-container' en el HTML.");
   }
