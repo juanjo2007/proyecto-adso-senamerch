54ca48bbe74501160c33b8b2584181f5e4063e92
msarias <=>
2025-12-01 17:32:27 -0500

    Fix Emanuel: Coreccion html

--- frontend/public/js/components/profile_store_seller.js
+++ frontend/public/js/components/profile_store_seller.js
@@ -1,4 +1,4 @@
-// ================== SALES COMPONENT ================== 
+// ================== SALES COMPONENT ==================
 document.addEventListener('DOMContentLoaded', function () {
   const profileclientContainer = document.querySelector('.profile-store-seller');
 
@@ -11,16 +11,40 @@ document.addEventListener('DOMContentLoaded', function () {
       .then(data => {
         profileclientContainer.innerHTML = data;
 
-        // ================== Evento para botones "Gestionar" ==================
-        const manageButtons = profileclientContainer.querySelectorAll('.store-profile__edit');
+        // ================== BOTONES EDITAR ==================
+        const editStoreBtn = profileclientContainer.querySelector('.store-profile__submit--store');
+        const editSellerBtn = profileclientContainer.querySelector('.store-profile__submit--seller');
 
-        manageButtons.forEach(button => {
-          button.addEventListener('click', () => {
+        if (editStoreBtn) {
+          editStoreBtn.addEventListener('click', () => {
             window.location.href = "edit_store_seller.html";
           });
-        });
+        }
+
+        if (editSellerBtn) {
+          editSellerBtn.addEventListener('click', () => {
+            window.location.href = "edit_seller_profile_view.html";
+          });
+        }
+
+        // ================== BOTONES VOLVER ==================
+        const backStoreBtn = profileclientContainer.querySelector('.store-profile__session--store');
+        const backSellerBtn = profileclientContainer.querySelector('.store-profile__session--seller');
+
+        if (backStoreBtn) {
+          backStoreBtn.addEventListener('click', () => {
+            window.location.href = "profile_store_seller.html";
+          });
+        }
+
+        if (backSellerBtn) {
+          backSellerBtn.addEventListener('click', () => {
+            window.location.href = "profile_store_seller.html";
+          });
+        }
+
       })
-      .catch(error => console.error("Error cargando el componente sales:", error));
+      .catch(error => console.error("Error cargando el componente profile_store_seller:", error));
   } else {
     console.warn("No se encontró '.profile-store-seller' en el HTML.");
   }
